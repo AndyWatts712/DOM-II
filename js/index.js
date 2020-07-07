4d6d416a683dab6720c7ee0ab2dbc658bc730a70
megawatts68 <andrew.f.watts@gmail.com>
2020-07-08 05:41:27 +0700

    Last 2 steps to go.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
 // Your code goes here
+const image = document.querySelector('img');
+image.addEventListener('mouseover', (e) => { e.target.style.border = "3px solid black"; });
+
+const body = document.querySelector('body');
+body.addEventListener('keydown', function (e) {
+    e.target.style.backgroundColor = "gray";
+});
+
+const header = document.querySelector('header');
+const changHeader = function () {
+    header.style.backgroundColor = "orange";
+};
+header.addEventListener('wheel', changHeader);
+
+const images = document.querySelectorAll('img');
+images.forEach(function (item) {
+    item.addEventListener('drag', function (e) {
+        e.target.style.border = "3px solid red"
+    });
+});
+
+header.addEventListener('dblclick', function(e) {
+    e.target.style.borderBottom = "5px solid blue"
+});
+
+images.forEach(function(item) {
+    item.addEventListener('dragend', function(){
+    header.style.backgroundColor = "yellow"
+    });
+});
+
+const homeLink = document.querySelector('a');
+const h1 = document.querySelector('h1');
+homeLink.addEventListener('click', function() {
+    h1.style.fontSize = "5rem"
+});
+
+const footer = document.querySelector('.footer');
+footer.addEventListener('mouseleave', function () {
+    footer.style.backgroundColor = "aqua"
+});
+
+const footerText = document.querySelector('.footer p');
+footerText.addEventListener('mouseup', function () {
+    footer.style.backgroundColor = "blue"
+});
